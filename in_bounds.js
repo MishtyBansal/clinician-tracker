355370add4b70c50216635324e5c8ebc7c07a0bc
MishtyBansal <mishtybansal3@gmail.com>
2026-03-13 16:52:09 -0400

    calling API and calculating point in polygon with and without API

--- in_bounds.js
+++ in_bounds.js
@@ -1,1 +1,37 @@
+import booleanPointInPolygon from "@turf/boolean-point-in-polygon";
+import { getClinicianData } from "./retrieve_data.js";
+
 // checks if the phlebotomist is within the bounds of their geoJSON range
+function pointInPolygonCalculation(point, polygon) {
+  console.log("point", point, "polygon", polygon);
+  let x = point[0];
+  let y = point[1];
+  let inside = 0;
+  for (let i = 0; i < polygon.length - 1; i++) {
+    let i2 = i + 1;
+    let coord1 = polygon[i];
+    let coord2 = polygon[i2];
+    // prettier-ignore
+    if ((coord1[1] > y) !== (coord2[1] > y)) {
+      let intersection_x =
+        ((coord1[0] - coord2[0]) * (y - coord2[1])) / (coord1[1] - coord2[1]) +
+        coord2[0];
+      if (intersection_x > x) {
+        inside++;
+      }
+    }
+  }
+  return inside % 2 != 0;
+}
+
+const data = await getClinicianData();
+for (let i = 0; i < data.length; i++) {
+  const point = data[i].features[0].geometry.coordinates;
+  const polygon = data[i].features[1].geometry.coordinates;
+  //   console.log("point", point, "polygon", polygon);
+  let inside = booleanPointInPolygon(data[i].features[0], data[i].features[1]);
+  // ^^ may have multiple regions -- need to test them all
+  let inBounds = pointInPolygonCalculation(point, polygon[0]);
+  console.log("with package", inside);
+  console.log("my calculation", inBounds);
+}
--- retrieve_data.js
+++ retrieve_data.js
@@ -1,1 +1,56 @@
 // will retrieve geoJSON for the 6 clinicians and return an array that stores them
+
+async function fetchData(baseURL, clinicianID) {
+  //   console.log(clinicianID, baseURL);
+  const url = `${baseURL}/clinicianstatus/${clinicianID}`;
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+      "Content-Type": "application/json",
+    });
+    if (!response.ok) {
+      console.log("in here");
+      throw new Error(`Status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
+}
+
+export async function getClinicianData() {
+  const baseURL = "https://3qbqr98twd.execute-api.us-west-2.amazonaws.com/test";
+  let data = [];
+  for (let i = 1; i < 7; i++) {
+    const userData = await fetchData(baseURL, i);
+    testValidData(userData);
+    data.push(userData);
+  }
+  return data;
+}
+
+function testValidData(data) {
+  if (typeof data !== "object" || data == null)
+    throw new Error("Invalid object: not a GeoJSON object");
+  let features = data.features;
+  for (let i = 0; i < features.length; i++) {
+    if (!features[i].type || features[i].type !== "Feature") {
+      throw new Error("Invalid object: each feature must have type 'Feature'");
+    }
+    if (
+      !features[i].geometry ||
+      !features[i].geometry.type ||
+      !features[i].geometry.coordinates
+    ) {
+      throw new Error(
+        "Invalid object: feature missing geometry/type/coordinates"
+      );
+    }
+    const type = features[i].geometry.type;
+    if (type !== "Point" && type !== "Polygon")
+      throw new Error(
+        "Invalid object: each object must have one Point and one Polygon"
+      );
+  }
+}
