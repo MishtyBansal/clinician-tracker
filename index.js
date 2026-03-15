31225b60937a37424446f1d4fec86d090a431e53
MishtyBansal <mishtybansal3@gmail.com>
2026-03-15 15:13:48 -0400

    changed structure; added state to track how long a clinician is out of bounds

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 import { driver } from "./driver.js";
 
+driver();
+
 setInterval(driver, 60000);
+
+setTimeout(() => {
+  clearInterval();
+  console.log("program has run for exactly 1 hour");
+  process.exit(0);
+}, 3600000);
--- retrieveData.js
+++ retrieveData.js
@@ -1,6 +1,32 @@
 // will retrieve geoJSON for the 6 clinicians and return an array that stores them
 
-async function retrieveData(baseURL, clinicianID) {
+const baseURL = "https://3qbqr98twd.execute-api.us-west-2.amazonaws.com/test";
+
+export async function getClinicianData(test) {
+  let data = [];
+  if (!test) {
+    for (let i = 1; i < 7; i++) {
+      const userData = await retrieveData(i);
+      try {
+        validateGeoJSONResponse(userData);
+        data.push({ ID: i, data: userData });
+      } catch (error) {
+        console.log(
+          "Skipping clinician",
+          clinicianIDs[i],
+          "- invalid data:",
+          err.message
+        );
+      }
+    }
+  } else {
+    const testingData = await retrieveData(baseURL, 7);
+    data.push({ ID: 7, data: testingData });
+  }
+  return data;
+}
+
+async function retrieveData(clinicianID) {
   const url = `${baseURL}/clinicianstatus/${clinicianID}`;
   try {
     const response = await fetch(url, {
@@ -8,7 +34,6 @@ async function retrieveData(baseURL, clinicianID) {
       "Content-Type": "application/json",
     });
     if (!response.ok) {
-      console.log("in here");
       throw new Error(`Status: ${response.status}`);
     }
     const data = await response.json();
@@ -18,32 +43,13 @@ async function retrieveData(baseURL, clinicianID) {
   }
 }
 
-export async function getClinicianData(test) {
-  const baseURL = "https://3qbqr98twd.execute-api.us-west-2.amazonaws.com/test";
-  let data = [];
-  if (!test) {
-    for (let i = 1; i < 7; i++) {
-      const userData = await retrieveData(baseURL, i);
-      validateGeoJSONResponse(userData);
-      data.push({ ID: i, data: userData });
-    }
-  } else {
-    const testingData = await retrieveData(baseURL, 7);
-    data.push({ ID: 7, data: testingData });
-  }
-  return data;
-}
-
-// RENAME: validateGeoJSONResponse
-// check if the turf package that validates the code automatically (so i dont have to do it manually)
 function validateGeoJSONResponse(data) {
   if (typeof data !== "object" || data == null)
     throw new Error("Invalid object: not a GeoJSON object");
   let features = data.features;
-  if (features === undefined)
-    throw new Error(
-      "Invalid Object: each GeoJSON object must have at least 1 feature."
-    );
+  if (!Array.isArray(features) || features.length === 0) {
+    throw new Error("Invalid Object: features array is missing or empty");
+  }
   for (let i = 0; i < features.length; i++) {
     if (!features[i].type || features[i].type !== "Feature") {
       throw new Error("Invalid object: each feature must have type 'Feature'");
